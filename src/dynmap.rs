c4337eb7638848b14267364ce99c51bd4c5280e2
Martijn Heil <m.heil375@gmail.com>
2024-10-28 00:03:06 +0100

    Make it work

--- src/dynmap.rs
+++ src/dynmap.rs
@@ -1,31 +1,81 @@
+use bytes::Bytes;
+use futures_core::stream::Stream;
+use reqwest::Client;
+use reqwest::header::ACCEPT;
+use reqwest;
+use rocket::http::ContentType;
 use serde::{Deserialize};
 
-#[derive(Deserialize)]
-struct Map {
-    name: String,
-    title: String,
-    prefix: String,
-    perspective: String,
-    scale: f64,
-    mapzoomout: u64,
-    image_format: String
+pub struct Dynmap {
+    client: Client,
+    base_url: String
 }
 
-#[derive(Deserialize)]
-struct World {
-    name: String,
-    title: String,
-    maps: Vec<Map>
+impl Dynmap {
+    pub fn new(base_url: String) -> Self {
+        Dynmap {
+            client: Client::new(),
+            base_url: base_url
+        }
+    }
+
+    pub async fn get_config(&self) -> Result<Configuration, Box<dyn std::error::Error>> {
+        let resp = reqwest::get(format!("{}{}", self.base_url, "/up/configuration"))
+            .await?
+            .json::<Configuration>()
+            .await?;
+
+        Ok(resp)
+    }
+
+    pub async fn get_tile(&self, world: &str, map: &str, zoom: u64, x: i64, z: i64, image_format: &str) 
+    -> Result<impl Stream<Item = reqwest::Result<Bytes>>, Box<dyn std::error::Error>> {
+        let content_type = ContentType::from_extension(&image_format).unwrap().to_string();
+        let url = format!("{}/tiles/{}/{}/0_0/{}{}_{}.{}", self.base_url, world, map, zoom_prefix(zoom), x, z, image_format);
+        let resp = self.client.get(url)
+            .header(ACCEPT, content_type)
+            .send()
+            .await?
+            .bytes_stream();
+
+        Ok(resp)
+    }
 }
 
-#[derive(Deserialize)]
-struct Configuration {
-    worlds: Vec<World>
+#[derive(Debug, Deserialize)]
+pub struct Map {
+    pub name: String,
+    pub title: String,
+    pub prefix: String,
+    pub perspective: String,
+    pub scale: u64,
+    pub tilescale: u64, // see tile_size()
+    pub mapzoomout: u64,
+    #[serde(rename = "image-format")] 
+    pub image_format: String // This is always a file extension, not a content type
 }
 
-async fn get_dynmap_config(base_url: &str) {
-    let resp = reqwest::get(fmt!("{}{}", base_url, "/up/configuration"))
-        .await?
-        .json::<DynmapConfiguration>()
-        .await?;
+#[derive(Debug, Deserialize)]
+pub struct World {
+    pub name: String,
+    pub title: String,
+    pub maps: Vec<Map>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Configuration {
+    pub worlds: Vec<World>
+}
+
+pub fn tile_size(tilescale: u64) -> u64 { 128 << tilescale }
+
+fn zoom_prefix(zoom: u64) -> String {
+    if zoom == 0 { return String::new(); }
+
+		// zoom == 0 -> ''
+		// zoom == 1 -> 'z_'
+		// zoom == 2 -> 'zz_'
+		let mut tmp = String::from(&"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"[..(zoom as usize)]);
+    tmp.push_str("_");
+    return tmp
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,183 @@
 #[macro_use] extern crate rocket;
-use rocket::http::{ContentType};
-use serde_json::json;
-use handlebars::Handlebars;
+use std::collections::HashSet;
+use std::path::Path;
+use bytes::Bytes;
 use dotenv::dotenv;
+use handlebars::Handlebars;
+use ordered_float::NotNan;
+use rocket::State;
+use rocket::http::ContentType;
+use rocket::http::Status;
+use rocket::response::stream::ReaderStream;
+use serde::{Serialize};
+use tokio_util::io::StreamReader;
+
+mod dynmap;
+
+use dynmap::Dynmap;
 
 const CAPABILITIES_TEMPLATE: &str = include_str!("capabilities.xml");
+const PIXEL_SIZE: f64 = 0.00028;
+const BASE_BLOCKS_PER_TILE: u64 = 32;
+const WORLD_SIZE: u64 = i16::MAX as u64;
+const TOP_LEFT: (i64, i64) = (0 - WORLD_SIZE as i64 / 2 , 0 - (0 - WORLD_SIZE as i64 / 2 - 32));
 
-#[get("/WMTSCapabilities.xml")]
-fn get_capabilities() -> (ContentType, String) {
-    let reg = Handlebars::new();
-    let base_url = std::env::var("BASE_URL").expect("BASE_URL must be set.");
-    let capabilities = reg.render_template(CAPABILITIES_TEMPLATE, &json!({"base_url": base_url, "worlds": [{"name": "testworld"}]}));
-
-    (ContentType::XML, capabilities.unwrap())
+#[derive(Debug, Serialize)]
+struct Capabilities {
+    base_url: String,
+    layers: Vec<Layer>,
+    tile_matrix_sets: HashSet<TileMatrixSet>
 }
 
-#[get("/tiles/<tile_matrix>/<tile_col>/<tile_row>")]
-fn get_tile(tile_matrix: i64, tile_col: i64, tile_row: i64) -> &'static str {
-    "test"
+#[derive(Debug, Serialize)]
+struct Layer {
+    identifier: String,
+    title: String,
+    tile_content_type: String,
+    tile_url_template: String
+}
+
+#[derive(Debug, Serialize, Eq, PartialEq, Hash)]
+struct TileMatrix {
+    identifier: String,
+    tile_size: u64,
+    scale_denominator: NotNan<f64>,
+    top_left_corner: (i64, i64),
+    matrix_size: u64
+}
+
+#[derive(Debug, Serialize, Eq, PartialEq, Hash)]
+struct TileMatrixSet {
+    identifier: String,
+    title: String,
+    matrices: Vec<TileMatrix>
+}
+
+fn scale_denominator(tile_size: u64, zoomout: u64) -> f64 {
+    let scale = (tile_size as f64) * PIXEL_SIZE / BASE_BLOCKS_PER_TILE as f64;
+    return 1.0 / scale * 2_f64.powf(zoomout as f64);
+}
+
+fn matrix_size(zoomout: u64) -> u64 {
+    return (WORLD_SIZE as f64 / BASE_BLOCKS_PER_TILE as f64 / 2f64.powf(zoomout as f64)).ceil() as u64;
+}
+
+impl dynmap::Configuration {
+    pub fn get_tile_matrix_sets(self: &dynmap::Configuration) -> HashSet<TileMatrixSet> {
+        let mut tile_matrix_sets: HashSet<TileMatrixSet> = HashSet::new();
+
+        for world in self.worlds.iter() {
+            for map in world.maps.iter() {
+                let mut matrices: Vec<TileMatrix> = Vec::new();
+                let tile_size = dynmap::tile_size(map.tilescale);
+                let max_zoomout = map.mapzoomout;
+
+                for zoomout in 0..=max_zoomout {
+                    matrices.push(TileMatrix {
+                        identifier: format!("{}", zoomout),
+                        tile_size: tile_size,
+                        scale_denominator: NotNan::new(scale_denominator(tile_size, zoomout)).unwrap(),
+                        top_left_corner: TOP_LEFT,
+                        matrix_size: matrix_size(zoomout)
+                    });
+                }
+
+                tile_matrix_sets.insert(TileMatrixSet {
+                    identifier: format!("{}_{}", &world.name, &map.name),
+                    title: format!("{} {}", &world.title, &map.title),
+                    matrices: matrices
+                });
+            }
+        }
+
+        tile_matrix_sets
+    }
+}
+
+#[get("/WMTSCapabilities.xml")]
+async fn get_capabilities(dynmap: &State<Dynmap>) -> Result<(ContentType, String), Status> {
+    let base_url = std::env::var("BASE_URL").expect("BASE_URL must be set.");
+
+    let config = dynmap.get_config().await.unwrap();
+
+    let capabilities = Capabilities {
+        base_url: base_url.clone(),
+        tile_matrix_sets: config.get_tile_matrix_sets(),
+        layers: config.worlds.into_iter().flat_map(|world| world.maps.into_iter().map(move |map| {
+            let identifier = format!("{}_{}", &world.name, &map.name);
+            Layer {
+                identifier: identifier.clone(),
+                title: format!("{} {}", &world.title, &map.title),
+                tile_content_type: ContentType::from_extension(&map.image_format).unwrap().to_string(),
+                tile_url_template: format!("{}/tiles/{}/{}/{{TileMatrix}}/{{TileCol}}/{{TileRow}}.{}", 
+                    std::env::var("BASE_URL").unwrap(),
+                    &world.name,
+                    &map.name,
+                    &map.image_format
+                )
+            }
+        })).collect()
+    };
+
+    let reg = Handlebars::new();
+    let response = reg.render_template(CAPABILITIES_TEMPLATE, &capabilities);
+    Ok((ContentType::Text, response.unwrap()))
+}
+
+#[get("/tiles/<world>/<map>/<tile_matrix>/<tile_col>/<file>")]
+async fn get_tile(
+    dynmap: &State<Dynmap>,
+    world: &str,
+    map: &str,
+    tile_matrix: u64,
+    tile_col: i64,
+    file: &str) -> Result<(ContentType, ReaderStream![StreamReader<impl rocket::futures::Stream<Item = Result<Bytes, std::io::Error>>, Bytes>]), Status> {
+    let file_path = Path::new(file);
+    let extension = match file_path.extension() {
+        Some(x) => match x.to_str() {
+            Some(s) => s,
+            None => return Err(Status::UnprocessableEntity)
+        },
+        None => return Err(Status::UnprocessableEntity)
+    };
+    let stem = match file_path.file_stem() {
+        Some(x) => match x.to_str() {
+            Some(s) => s,
+            None => return Err(Status::UnprocessableEntity)
+        },
+        None => return Err(Status::UnprocessableEntity)
+    };
+    let tile_row: i64 = match stem.parse() {
+        Ok(tile_row) => tile_row,
+        Err(_) => return Err(Status::UnprocessableEntity)
+    };
+    let content_type = match ContentType::from_extension(&extension) {
+        Some(v) => v,
+        None => return Err(Status::UnprocessableEntity)
+    };
+
+    let matrix_size = matrix_size(tile_matrix);
+    let zoom_ratio = 2i64.pow(tile_matrix as u32);
+    let x = (tile_col - (matrix_size as i64) / 2) * zoom_ratio;
+    let y = (tile_row - (matrix_size as i64) / 2) * zoom_ratio;
+    let z = -y;
+
+    let stream = dynmap.get_tile(&world, &map, tile_matrix, x, z, &extension).await.unwrap();
+
+    use rocket::futures::TryStreamExt; // for map_err() call below:
+    let reader = StreamReader::new(stream.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e)));
+    Ok((content_type, ReaderStream::one(reader)))
 }
 
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
-    rocket::build().mount("/", routes![get_capabilities, get_tile])
+
+    let dynmap_url = std::env::var("DYNMAP_URL").expect("DYNMAP_URL must be set.");
+    let dynmap = Dynmap::new(dynmap_url);
+
+    rocket::build()
+        .manage(dynmap)
+        .mount("/", routes![get_capabilities, get_tile])
 }
 
